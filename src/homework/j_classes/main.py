1ba0358ae39d28a7f858eda4623b55d950d0fdeb
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-03 23:34:40 -0500

    Update main.py

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -1,1 +1,13 @@
-#
+from src.homework.j_classes.class_a import Die
+
+def main():
+    die = Die()
+    while True:
+        die.roll()
+        print(die)
+        choice = input("Roll again? (y/n): ").strip().lower()
+        if choice != 'y':
+            break
+
+if __name__ == "__main__":
+    main() #
