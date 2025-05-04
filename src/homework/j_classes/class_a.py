5013e419439672978bfb103aca9f6a6eff26dc2f
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-03 23:32:01 -0500

    Update class_a.py

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,11 @@
-#
+#import random
+
+class Die:
+    def __init__(self):
+        self.__roll_value = None
+
+    def roll(self):
+        self.__roll_value = random.randint(1, 6)
+
+    def get_rolled_value(self):
+        return self.__roll_value
