83f77f85858a64c096ac6d0d1e0925c6e90b0d99
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-03 23:33:50 -0500

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,5 @@
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.b_in_proc_out import tests_in_proc_out
+from tests.homework.j_classes import tests_classes
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
-unittest.TextTestRunner(verbosity=2).run(suite)
+suite = unittest.TestLoader().loadTestsFromModule(tests_classes)
+unittest.TextTestRunner().run(suite)
