537b1e934102ca20b7ad3998f8f3f4e81778cb73
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-11 14:56:44 -0500

    Update lists.py

--- src/homework/g_lists_and_tuples/lists.py
+++ src/homework/g_lists_and_tuples/lists.py
@@ -1,17 +1,13 @@
-def get_p_distance(list1, list2):
-    differences = 0
-    for a, b in zip(list1, list2):
-        if a != b:
-            differences += 1
-    return differences / len(list1)
+def get_lowest_list_value(numbers):
+    lowest = numbers[0]
+    for num in numbers:
+        if num < lowest:
+            lowest = num
+    return lowest
 
-
-def get_p_distance_matrix(dna_lists):
-    size = len(dna_lists)
-    matrix = [[0.0] * size for _ in range(size)]
-
-    for i in range(size):
-        for j in range(size):
-            if i != j:
-                matrix[i][j] = round(get_p_distance(dna_lists[i], dna_lists[j]), 5)
-    return matrix
+def get_highest_list_value(numbers):
+    highest = numbers[0]
+    for num in numbers:
+        if num > highest:
+            highest = num
+    return highest
