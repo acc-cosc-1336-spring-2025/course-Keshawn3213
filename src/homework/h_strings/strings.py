67ba7ce5a9dfc43387b277cb8ce3a61b00a44a8f
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-11 13:59:14 -0500

    Update strings.py

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,20 @@
-#
+def get_hamming_distance(dna1, dna2):
+    distance = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            distance += 1
+    return distance
+
+def get_dna_complement(dna):
+    complement = ''
+    for i in range(len(dna) - 1, -1, -1):
+        base = dna[i]
+        if base == 'A':
+            complement += 'T'
+        elif base == 'T':
+            complement += 'A'
+        elif base == 'C':
+            complement += 'G'
+        elif base == 'G':
+            complement += 'C'
+
