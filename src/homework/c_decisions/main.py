9f97f0945c45d55c9f87811ac0d1d417344c04bf
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-11 11:57:19 -0500

    Update main.py

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,11 @@
-#
+#import decisions
+
+def main():
+    options = int(input("Enter the number of options: "))
+    total = int(input("Enter the total number of responses: "))
+    ratio = decisions.get_options_ratio(options, total)
+    rating = decisions.get_faculty_rating(ratio)
+    print(f"Faculty rating is: {rating}")
+
+if __name__ == "__main__":
+    main()
