b9c8b62a5cf9012de65f30422bb1eb9f9dec4be1
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-11 14:58:21 -0500

    Update tests_lists_and_tuples.py

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,24 +1,13 @@
 import unittest
-from src.homework.g_lists_and_tuples.lists import get_p_distance, get_p_distance_matrix
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value, get_highest_list_value
 
 class Test_Config(unittest.TestCase):
 
-    def test_p_distance(self):
-        list1 = ['T','T','T','C','C','A','T','T','T','A']
-        list2 = ['G','A','T','T','C','A','T','T','T','C']
-        self.assertAlmostEqual(get_p_distance(list1, list2), 0.4, places=3)
+    def test_get_lowest_list_value(self):
+        data = [8, 10, 1, 50, 20]
+        self.assertEqual(get_lowest_list_value(data), 1)
 
-    def test_get_p_distance_matrix(self):
-        data = [
-            ['T','T','T','C','C','A','T','T','T','A'],
-            ['G','A','T','T','C','A','T','T','T','C'],
-            ['T','T','T','C','C','A','T','T','T','T'],
-            ['G','T','T','C','C','A','T','T','T','A']
-        ]
-        expected = [
-            [0.0, 0.4, 0.1, 0.1],
-            [0.4, 0.0, 0.4, 0.3],
-            [0.1, 0.4, 0.0, 0.2],
-            [0.1, 0.3, 0.2, 0.0]
-        ]
+    def test_get_highest_list_value(self):
+        data = [8, 10, 1, 50, 20]
+        self.assertEqual(get_highest_list_value(data), 50)
   
