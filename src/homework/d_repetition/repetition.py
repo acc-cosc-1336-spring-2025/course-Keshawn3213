48489fc746930d36f0f708eba1c2c4b05f6c13a3
Keshawn3213 <keshawn.poole@g.austincc.edu>
2025-05-11 12:02:08 -0500

    Update repetition.py

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,14 @@
-#
+def get_factorial(num):
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
+
+def sum_odd_numbers(num):
+    total = 0
+    i = 1
+    while i <= num:
+        if i % 2 != 0:
+            total += i
+        i += 1
+    return total#
